b4f52ae6ce03023969430523267d520001f6c4ae
Михаил Перетятько <mikhail.peretyatko@mpstats.io>
2026-08-21 00:58:10 +0300

    Поправил порт

--- src/env.js
+++ src/env.js
@@ -1,5 +1,5 @@
 export default {
-  host: 'https://127.0.0.1:8792',
+  host: 'https://127.0.0.1:8793',
   // host: 'https://188.227.32.83',
   // host: 'https://amocrm2.gospodaprogrammisty.ru',
   widget_name: 'SMARTWAY:LAST_TOUCH',
